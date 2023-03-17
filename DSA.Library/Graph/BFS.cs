5c5a4219ecd53bdb18c39a1e9bfdc6e16fa15719
Elio Cortes <elio.cortes3000@gmail.com>
2023-03-17 18:40:49 -0500

    Created Class Library Project Created xUnit Tests Project Documented Big-O notation Defined first BFS Graph Clases and Unit Test

---
+++ DSA.Library/Graph/BFS.cs
@@ -0,0 +1,17 @@
+﻿namespace DSA.Library.Graph
+{
+    public class BFS
+    {
+        private Graph graph;
+
+        public BFS(Graph graph)
+        {
+            this.graph = graph;
+        }
+
+        public int[] Traverse(int v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DSA.Tests/BFSTests.cs
@@ -0,0 +1,22 @@
+﻿namespace DSA.Tests
+{
+    public class BFSTests
+    {
+        public void ValidateBreadthFirstTraversal()
+        {
+            Graph graph = new Graph(4);
+
+            graph.AddEdge(0, 1);
+            graph.AddEdge(0, 2);
+            graph.AddEdge(1, 2);
+            graph.AddEdge(2, 0);
+            graph.AddEdge(2, 3);
+            graph.AddEdge(3, 3);
+            
+            BFS bFS = new BFS(graph);
+            int[] traverse= bFS.Traverse(2);
+            int[] expected = { 2, 0, 3, 1 };
+            Assert.Equal(expected, traverse);
+        }
+    }
+}
