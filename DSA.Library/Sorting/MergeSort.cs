c81144f5312b622cb6e6bcd16de8396561cd3654
Elio Cortés <elio.cortes3000@gmail.com>
2023-03-25 12:23:53 -0500

    Integrated sorting changes

---
+++ DSA.Library/Sorting/MergeSort.cs
@@ -0,0 +1,63 @@
+﻿namespace DSA.Library.Sorting
+{
+    public class MergeSort
+    {
+        private int[] inputArray;
+
+        public MergeSort(int[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
+
+        public int[] Execute()
+        {
+            SortSubArray(0, inputArray.Length);
+            return inputArray;
+        }
+
+        private void SortSubArray(int low, int high)
+        {
+            if (low < high)
+            {
+                int middle = low + (high - low) / 2;
+                if (middle > 0)
+                {
+                    SortSubArray(low, middle);
+                    SortSubArray(middle + 1, high);
+                    Merge(low, middle, high);
+                }
+                
+            }
+        }
+
+        private void Merge(int low, int middle, int high)
+        {
+            int leftLength = middle - low + 1;
+            int rightLength = high - middle;
+            var leftArray = inputArray[leftLength..middle];
+            var rightArray = inputArray[middle..rightLength];
+            int i = 0;
+            int j = 0;
+            int k = low;
+            while (i < leftLength && j < rightLength)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    inputArray[k++] = leftArray[i++];
+                }
+                else
+                {
+                    inputArray[k++] = rightArray[j++];
+                }
+            }
+            while (i < leftLength)
+            {
+                inputArray[k++] = leftArray[i++];
+            }
+            while (j < rightLength)
+            {
+                inputArray[k++] = rightArray[j++];
+            }
+        }
+    }
+}
